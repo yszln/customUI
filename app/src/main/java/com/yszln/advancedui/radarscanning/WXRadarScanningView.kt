c9f39e29e73fb568a1df62595bc67a159a8eaac7
huwei <1686876764@qq.com>
2020-06-11 14:16:14 +0800

    仿微信附近好友雷达扫描

--- app/src/main/java/com/yszln/advancedui/radarscanning/WXRadarScanningView.kt
+++ app/src/main/java/com/yszln/advancedui/radarscanning/WXRadarScanningView.kt
@@ -1,11 +1,10 @@
 package com.yszln.advancedui.radarscanning
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.yszln.advancedui.R
 import kotlin.math.min
 
 /**
@@ -17,8 +16,8 @@ import kotlin.math.min
  */
 class WXRadarScanningView @JvmOverloads constructor(
     context: Context?,
-    attrs: AttributeSet?=null,
-    defStyleAttr: Int=0
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
     /**
@@ -29,36 +28,89 @@ class WXRadarScanningView @JvmOverloads constructor(
 
     private var mPaint = Paint()
 
+    private var mWidth = 0;
+    private var mHeight = 0;
+    private var mCenterX = 0f;
+    private var mCenterY = 0f;
+    private var mCircleRadius = 0f;
+    private var mBitmap: Bitmap? = null
+
+    /**
+     * 扫描旋转的角度
+     */
+    private var mRoteDegree = 0f;
+
+    private var mRoteMatrix = Matrix()
+
+
+    private var mRun = object : Runnable {
+        override fun run() {
+            mRoteDegree += 2
+            mRoteMatrix.postRotate(mRoteDegree)
+            postDelayed(this, 60)
+            invalidate()
+        }
+    }
+
     init {
         mPaint.isAntiAlias = true
         mPaint.color = Color.LTGRAY
         mPaint.style = Paint.Style.STROKE
+        mBitmap = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
+        mRun.run()
     }
 
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        canvas.save()
         drawCircle(canvas)
-
+        drawBitmap(canvas)
+        canvas.restore()
     }
 
-    /**
-     * 绘制6个圆
-     */
-    private fun drawCircle(canvas: Canvas) {
-
-        for (fl in circleProportion) {
-            canvas.drawCircle(
-                measuredWidth / 2f,
-                measuredHeight / 2f,
-                min(measuredWidth, measuredHeight).toFloat()/2 * fl,
-                mPaint
+    private fun drawBitmap(canvas: Canvas) {
+        mBitmap?.let {
+            canvas.drawBitmap(
+                it,
+                mCenterX - it.width / 2,
+                mCenterY - it.height / 2,
+                null
             )
         }
     }
 
+    /**z
+     * 绘制6个圆
+     */
+    private fun drawCircle(canvas: Canvas) {
+        mPaint.shader = SweepGradient(
+            mCenterX,
+            mCenterY,
+            intArrayOf(Color.TRANSPARENT, Color.parseColor("#84B5CA")),
+            null
+        )
+//        canvas.concat(mRoteMatrix)
+        for (fl in circleProportion) {
+            canvas.drawCircle(
+                mCenterX,
+                mCenterY,
+                mCircleRadius * fl,
+                mPaint
+            )
+        }
+
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        mWidth = measuredWidth
+        mHeight = measuredHeight
+        mCenterX = min(measuredWidth, measuredHeight) / 2f
+        mCenterY = mCenterX
+        mCircleRadius = mCenterX
+
+
     }
 
 
